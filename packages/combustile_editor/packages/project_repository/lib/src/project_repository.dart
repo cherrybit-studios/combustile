635aad89a35547e59c7763cd92d06159924456d9
Erick Zanardo <erickzanardoo@gmail.com>
2023-04-04 08:43:03 -0300

    feat: bootstrapin project repository

---
+++ packages/combustile_editor/packages/project_repository/lib/src/project_repository.dart
@@ -0,0 +1,7 @@
+/// {@template project_repository}
+/// repository that allows to manipulate and read project files
+/// {@endtemplate}
+class ProjectRepository {
+  /// {@macro project_repository}
+  const ProjectRepository();
+}
---
+++ packages/combustile_editor/packages/project_repository/test/src/project_repository_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:project_repository/project_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('ProjectRepository', () {
+    test('can be instantiated', () {
+      expect(ProjectRepository(), isNotNull);
+    });
+  });
+}
