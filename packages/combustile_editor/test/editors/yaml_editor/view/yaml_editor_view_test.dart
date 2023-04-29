89f2ea37838555a717f393d22f73c112803fc811
Erick Zanardo <erickzanardoo@gmail.com>
2023-04-29 17:44:57 -0300

    lint

--- packages/combustile_editor/test/editors/yaml_editor/view/yaml_editor_view_test.dart
+++ packages/combustile_editor/test/editors/yaml_editor/view/yaml_editor_view_test.dart
@@ -2,7 +2,6 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:code_text_field/code_text_field.dart';
 import 'package:combustile_editor/editors/editors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
